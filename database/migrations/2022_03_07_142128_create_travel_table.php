3a92031de918552da89e1ef1f6e6c7c7d72afee2
Cezar <cezarlantosgmail.com>
2022-03-07 16:06:27 +0100

    create table

---
+++ database/migrations/2022_03_07_142128_create_travel_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel', function (Blueprint $table) {
+            $table->id();
+            $table->string("address", 50);
+            $table->string("city", 20);
+            $table->string("state", 20);
+            $table->string("country", 20);
+            $table->date("date of departure");
+            $table->date("date of arrival");
+            $table->float("total price", 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel');
+    }
+}
